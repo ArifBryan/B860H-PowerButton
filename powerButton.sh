72e0db7df65a33087aa011fb6872a814ded2321e
Arif Bryan <arief372@gmail.com>
2023-06-30 01:13:32 +0700

    Change to oneshot service

--- powerButton.sh
+++ powerButton.sh
@@ -3,17 +3,8 @@
 gpioset periphs-banks 73=0
 gpioset periphs-banks 75=1
 
-while :
-do
-  btn=$(gpiomon -f -n1 -F%e aobus-banks 2)
-
-  if [ $btn -eq 0 ]
-  then
-    poweroff
-
-    while :
-    do
-	  sleep 1
-    done
-  fi
-done
+btn=$(gpiomon -f -n1 -F%e aobus-banks 2)
+if [ $btn -eq 0 ]
+then
+  poweroff
+fi
