ee725610400a766c823c433e117ff4060d203684
Arif Bryan <arief372@gmail.com>
2024-04-03 18:32:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,10 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 cp powerButton.sh /usr/local/sbin/
-cp powerLedOff.sh /usr/local/sbin/
 
 cp powerButton.service /etc/systemd/system/
-cp powerLedOff.service /etc/systemd/system/
 
 systemctl enable powerButton.service
-systemctl enable powerLedOff.service
 
 systemctl daemon-reload
 systemctl start powerButton.service
