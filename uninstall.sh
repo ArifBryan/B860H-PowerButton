f17959a0b78b383b71956c95cbed492205357c53
Arif Bryan <arief372@gmail.com>
2024-04-03 18:31:32 +0800

    Remove power off red led

--- uninstall.sh
+++ uninstall.sh
@@ -8,13 +8,10 @@ fi
 systemctl stop powerButton.service
 
 systemctl disable powerButton.service
-systemctl disable powerLedOff.service
 
 rm -rf /etc/systemd/system/powerButton.service
-rm -rf /etc/systemd/system/powerLedOff.service
 
 rm -rf /usr/local/sbin/powerButton.sh
-rm -rf /usr/local/sbin/powerLedOff.sh
 
 systemctl daemon-reload
 
